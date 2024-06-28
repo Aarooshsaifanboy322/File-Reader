808231238570d7e294cc2282b33d7290b5f9241d
AarooshRBLX <164038746+Aarooshsaifanboy322@users.noreply.github.com>
2024-06-28 14:22:53 +0600

    Updated application code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import os
 import time
 
-# Code (Editable)
+# Code
 # Change Repository
 chrepos = input("First, change directory. Start of with '/Users/(Username)/(Folder or file)': ")
 print("Changing directory, please wait...")
@@ -14,12 +14,14 @@ time.sleep(6)
 try:
     os.chdir(chrepos)
     print("Changed Directory!")
-except:
+    # Main file reader
+    fileInput = input("Enter the file name you want to read. Please put the file extension also: ")
+    reader = open(fileInput, "r") # Opens the specified file
+    try:
+        # Reader
+        print(reader.read())
+    except Exception:
+        print("Couldn't read file. It might be an image, undecodable file or not readable file that you want to read.")
+except Exception:
     print("Couldn't change repositories. Please run the action again.")
     quit()
-
-# Main file reader
-fileInput = input("Enter the file name you want to read. Please put the file extension also: ")
-reader = open(fileInput, "r") # Opens the specified file
-# Reader
-print(reader.read())
